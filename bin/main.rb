06dc91a502ca73184fe119700d028894eca5e5ed
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2020-09-03 16:55:48 -0400

    Adding user interface description

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,23 @@
 #!/usr/bin/env ruby
 
-puts 'Hello World'
+# What we need to display for the player:
+
+# Instructions on how to play the game
+# The 3 by 3 grid Board:
+# 1 | 2 | 3
+# - | - | -
+# 4 | 5 | 6   Representation of the board
+# - | - | -
+# 7 | 8 | 9
+# Which player should make a move
+# Fill the board accordingly with the players' move
+# A warning message if the player makes an invalid move
+# A winning or draw message when the game is over
+# An Ending message after the game ends, ask the player
+#  if he wants to play again
+
+# Input from players:
+# Ask player 1 to select an empty place (1-9) to make
+#   his move then add an X in that place (if empty)
+# Ask player 2 to select an empty place (1-9) to make
+#   his move then add an O in that place (if empty)
