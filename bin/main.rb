16d6ab0e48904309ac734fd333c9273699e99f2a
Carlos-SSH <carlos.anezeh@me.com>
2020-09-03 16:26:19 -0500

    Added more instructions.

--- bin/main.rb
+++ bin/main.rb
@@ -9,6 +9,7 @@
 # 4 | 5 | 6   Representation of the board
 # - | - | -
 # 7 | 8 | 9
+
 # Which player should make a move
 # Fill the board accordingly with the players' move
 # A warning message if the player makes an invalid move
@@ -21,3 +22,30 @@
 #   his move then add an X in that place (if empty)
 # Ask player 2 to select an empty place (1-9) to make
 #   his move then add an O in that place (if empty)
+
+# Valid Moves:
+# if the players selects a place not taken puts his symbol in this place.
+# if the players selects a taken place returns the warning message.
+
+# How to win:
+# Any row has the same symbol on its 3 spaces:
+# 1,2,3 Horizontal Top
+# 4,5,6 Horizontal middle
+# 7,8,9 Horizontal bottom
+
+# Any column has the same symbol on its 3 spaces:
+# 1,4,7 Vertical left
+# 2,5,8 Vertical center
+# 3,6,9 Vertical right
+
+# Any diagonal has the same symbol on its 3 spaces:
+# 1,5,9 Diagonal Left
+# 3,5,7 Diagonal right
+
+# How to end the game:
+# One of the player win
+#  Display (Winner, Winner, Chicken Dinner!!)
+# Current player can't make a move
+#  Display a draw message (It's a draw)
+#
+# Display a message ask the players if they want to play again (Y/N)
