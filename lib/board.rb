1c0ec5f3f67de6faa26e33fc801848c66880ff3a
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2020-09-03 18:43:43 -0400

    Add board class and move method

---
+++ lib/board.rb
@@ -0,0 +1,27 @@
+class Board
+  attr_accessor :board
+
+  def initialize(board)
+    @board = board
+  end
+
+  def move_1
+    move = gets.chomp.to_i - 1
+    if @board[move].is_a? Numeric 
+      @board[move] = 'X'
+      return @board
+    else 
+      'Invalid move'
+    end
+  end
+
+  def move_2
+    move = gets.chomp.to_i - 1
+    if @board[move].is_a? Numeric 
+      @board[move] = 'O'
+      return @board
+    else 
+      'Invalid move'
+    end
+  end
+end
